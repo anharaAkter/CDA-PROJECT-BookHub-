8306ccb00401a31d6053bbd1575e0158c249df9b
anharaAkter <anhara.susoma@gmail.com>
2023-07-17 01:27:40 +0200

    addAuthor and UpdateAuthor method added to controller

--- backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Controllers/AuthorController.java
+++ backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Controllers/AuthorController.java
@@ -3,10 +3,7 @@ package com.bookhub.bookhubapi.Controllers;
 import com.bookhub.bookhubapi.Models.Author;
 import com.bookhub.bookhubapi.Services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +16,18 @@ public class AuthorController {
     public List<Author> getAllAuthor(){
         return authorService.getAllAuthor();
     }
+
+    @GetMapping("/author/{id}")
+    public Author getAuthorById(@PathVariable(value = "id") Integer id){
+        return authorService.getAuthorById(id);
+    }
     @PostMapping("/author")
     public void addAuthor(@RequestBody Author author){
         authorService.addAuthor(author);
+    }
+
+    @PostMapping("author/{id}")
+    public Author updateAuthor(@PathVariable("id") Integer id , @RequestBody Author author){
+        return authorService.updateAuthor(author,id);
     }
 }
--- backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Services/AuthorService.java
+++ backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Services/AuthorService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class AuthorService {
@@ -14,6 +16,30 @@ public class AuthorService {
     public List<Author> getAllAuthor(){
         return authorRepo.findAll();
     }
+
+    public Author getAuthorById(Integer id){
+        return authorRepo.findById(id).orElseThrow();
+    }
+    public Author updateAuthor(Author author, int id) {
+        // Check if the provided Author object is valid
+        if (author == null || author.getFirstNameAuthor() == null || author.getLastNameAuthor() == null) {
+            throw new IllegalArgumentException("Invalid Author data. All fields are required.");
+        }
+
+        // Find the existing author by id
+        Author existingAuthor = authorRepo.findById(id).orElseThrow(() -> new NoSuchElementException("Author with id " + id + " not found."));
+
+        // Update the existing author with the values from the input author
+        existingAuthor.setAuthorId(author.getAuthorId());
+        existingAuthor.setFirstNameAuthor(author.getFirstNameAuthor());
+        existingAuthor.setLastNameAuthor(author.getLastNameAuthor());
+
+        // Save the updated author to the database
+        existingAuthor = authorRepo.save(existingAuthor);
+
+        return existingAuthor;
+    }
+
 
     public void addAuthor(Author author) {
         authorRepo.save(author);
