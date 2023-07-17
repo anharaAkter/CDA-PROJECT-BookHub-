43055ce4fce1d6aac80b35ecc8907874c5e4f042
anharaAkter <anhara.susoma@gmail.com>
2023-07-17 09:36:44 +0200

    back/feat: category find and update method are added

---
+++ backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Controllers/CategoryController.java
@@ -0,0 +1,39 @@
+package com.bookhub.bookhubapi.Controllers;
+import com.bookhub.bookhubapi.Models.Category;
+
+import com.bookhub.bookhubapi.Services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/category/")
+    public void addCategory(@RequestBody Category category){
+        categoryService.addCategory(category);
+    }
+    @GetMapping("/categories")
+    public List<Category> getAllCategory(){
+        return categoryService.getAllCategory();
+    }
+
+    @GetMapping("/category/{id}")
+    public Category getCategoryById(@PathVariable(value = "id") Integer id){
+        return categoryService.getCategoryById(id);
+    }
+
+    //update category
+    @PostMapping("category/{id}")
+    public Category updateCategory(@PathVariable("id") Integer id , @RequestBody Category category){
+        return categoryService.updateCategory(category,id);
+    }
+
+
+
+
+
+}
---
+++ backend/BookHubApi/src/main/java/com/bookhub/bookhubapi/Services/CategoryService.java
@@ -0,0 +1,46 @@
+package com.bookhub.bookhubapi.Services;
+
+import com.bookhub.bookhubapi.Models.Author;
+import com.bookhub.bookhubapi.Models.Category;
+import com.bookhub.bookhubapi.Repositories.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepo categoryRepo;
+
+    public List<Category> getAllCategory() {
+        return categoryRepo.findAll();
+    }
+
+    public void addCategory(Category category) {
+        categoryRepo.save(category);
+    }
+
+    public Category getCategoryById(Integer id) {
+        return categoryRepo.findById(id).orElseThrow();
+    }
+
+    public Category updateCategory(Category category, Integer id) {
+        // Check if the provided category object is valid
+        if (category == null  || category.getNameCategory() == null) {
+            throw new IllegalArgumentException("Invalid Author data. All fields are required.");
+        }
+        // Find the existing category by id
+        Category existingCategory = categoryRepo.findById(id).orElseThrow(() -> new NoSuchElementException("Author with id " + id + " not found."));
+
+         // Update the existing category with the values from the input category
+        existingCategory.setCategoryId(category.getCategoryId());
+        existingCategory.setNameCategory(category.getNameCategory());
+
+        // Save the updated category to the database
+        existingCategory = categoryRepo.save(existingCategory);
+
+        return existingCategory;
+    }
+}
